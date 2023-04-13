1d68475388cb4a815cc0503fc8a31a4618202faa
Juraev <jakadef507@gmail.com>
2023-04-13 15:20:01 +0500

    added folder in gitignore file and created print statement in first class

--- src/Practice1/FirstClass.java
+++ src/Practice1/FirstClass.java
@@ -4,4 +4,7 @@ public class FirstClass {
 //Test 1 is done
 //Test 2 is done
 //all tests are completed
+public static void main(String[] args) {
+    System.out.println("Tests are completed");
+}
 }
